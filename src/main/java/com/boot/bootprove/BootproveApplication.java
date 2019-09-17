cfcbfd92133e023452d288bc1fda9006352d6234
wzj0916 <1336720773@qq.com>
2019-09-17 11:08:19 +0800

    init springboot improve

---
+++ src/main/java/com/boot/bootprove/BootproveApplication.java
@@ -0,0 +1,13 @@
+package com.boot.bootprove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootproveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootproveApplication.class, args);
+	}
+
+}
